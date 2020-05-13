9d8de4f62abaea1a5c445c61057653875443a1c2
wsy <poiuytrengo@qq.com>
2020-05-14 00:10:59 +0800

    add 001

---
+++ src/com/johnson/leetcode/easy/Solution.java
@@ -0,0 +1,4 @@
+package com.johnson.leetcode.easy;
+
+public class Solution {
+}
---
+++ src/com/johnson/leetcode/easy/_001/Solution.java
@@ -0,0 +1,42 @@
+package com.johnson.leetcode.easy._001;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @ClassName Solution
+ * @Description 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 nums = [2, 7, 11, 15], target = 9
+ * @Author Johnson
+ * @Date 2020年5月14日00:02:05
+ */
+
+public class Solution {
+    /*public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) return new int[]{i, j};
+            }
+        }
+        return null;
+    }*/
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; ++i) {
+            Integer value = map.get(nums[i]);
+            if (value != null) {
+                return new int[]{value, i};
+            }
+            map.put(target - nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+}
